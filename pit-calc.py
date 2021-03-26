92f3c9e6975831feef30f4504d2439c60ae541f0
EEKIM10 <eek@clicksminuteper.net>
2021-03-26 09:38:59 +0000

    Tidy up

--- pit-calc.py
+++ pit-calc.py
@@ -11,9 +11,7 @@ try:
     with open("./config.json") as config:
         cfg = json.load(config)
 except:
-    cfg = {
-        "log_location": None
-    }
+    cfg = {"log_location": None}
     with open("./config.json", "w+") as config:
         json.dump(cfg, config, indent=2)
     print("No config found. Please edit ./config.json#log_location to be the filepath to your endlessh logs.")
@@ -45,28 +43,19 @@ for n, line in enumerate(logdata):
             print("No opening ACCEPT was detected. Incomplete logs?")
         connected -= 1
     if not regex.search(line):
-
         continue
-
     try:
-
         time = float(regex.search(line).group()[5:])
-
     except Exception as e:
-
         print("Failed to parse line {}: {}".format(n, str(e)))
-
         continue
-
     avg.append(time)
-
     if time > longest_time:
-
         longest_time = time
-
     elif time < shortest_time:
-
         shortest_time = time
 
-average = round(sum(avg)/len(avg), 2)
-print(f"Longest Capture Time: {round(longest_time, 2)}s\nShortest Capture Time: {round(shortest_time, 2)}s\nAverage Capture Time: {average}s\nActive Clients: {connected}")
+average = round(sum(avg) / len(avg), 2)
+print(
+    f"Longest Capture Time: {round(longest_time, 2)}s\nShortest Capture Time: {round(shortest_time, 2)}s\nAverage Capture Time: {average}s\nActive Clients: {connected}"
+)
